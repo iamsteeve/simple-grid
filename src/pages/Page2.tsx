7f8d585bf173f02476392272ef4ec504032f6ecd
Steeve Luis Angel Torres Agustin <iam@steeve.me>
2018-12-01 12:45:15 -0500

    Cambios menores en paginas

--- src/pages/Page2.tsx
+++ src/pages/Page2.tsx
@@ -4,14 +4,14 @@ export default () => (
     <React.Fragment>
 
         <div className="row">
-            <div className="col-xs-12 col-md-8 col-lg-10 ">Column 1</div>
+            <div className="col-xs-12 col-md-8 col-lg-10 "><p>col-xs-12 col-md-8 col-lg-10</p> </div>
 
-            <div className="col-xs-12 col-md-4 col-lg-2 ">Column 2</div>
+            <div className="col-xs-12 col-md-4 col-lg-2 "><img className="fluid-img" src="https://via.placeholder.com/650" alt="a"/></div>
         </div>
 
         <div className="row">
             <div className="col-xs-12">
-                COlum12
+                <p>col-xs-12 </p>
             </div>
         </div>
     </React.Fragment>
